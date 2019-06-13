06b9567d9385b942afec0d80fe9fad746850f986
Aisa <aisajemila@gmail.com>
2019-06-13 19:03:00 +0300

    Added a base file for and configured static file in the settings file of project.

---
+++ web_dashboard/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.conf.urls import url,include
+
+#urls defination to navigate through the pages
+urlpatterns=[
+  url(r'^dashboard/',views.dashboard,name='dashboard')
+]
--- web_dashboard/views.py
+++ web_dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import Http404,HttpResponse
 
 # Create your views here.
 def dashboard(request):
