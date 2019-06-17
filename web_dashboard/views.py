d7d85cc944066fe8e92c8698ce9e26762d4392d4
Aisa <aisajemila@gmail.com>
2019-06-17 14:28:07 +0300

    Installed pyrebase and configured to connecting to the app by consuming the firebase database API.

--- web_dashboard/views.py
+++ web_dashboard/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http import Http404,HttpResponse
+import pyrebase
+
+config={
+  apiKey: "AIzaSyC0Z5dua996GPaJzlwX1aK_D6FcVSxUNSo",
+  authDomain: "moveme-147d2.firebaseapp.com",
+  databaseURL: "https://moveme-147d2.firebaseio.com",
+  projectId: "moveme-147d2",
+  storageBucket: "moveme-147d2.appspot.com",
+  messagingSenderId: "183677734527",
+  appId: "1:183677734527:web:3d6d59226b572354"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 # Create your views here.
 def dashboard(request):
@@ -17,3 +31,5 @@ def users(request):
 def drivers(request):
 
   return render(request,'drivers.html')
+
+def adminSignIn(re)
